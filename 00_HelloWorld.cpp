ac7e187d7e1d1de6c89d1a67f63057dcea1e9888
dargyris <diamantisargiris@gmail.com>
2020-04-21 14:57:42 +0300

    Cleaning up...

--- 00_HelloWorld.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-//Some changes from another developer here.
-int main(){
-    std::cout << "Hello world!\n";
-}
--- std_lib_facilities.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <unistd.h>
-
-using namespace std;
